5b06f13699b0d5ed62ee4f6cef507fbe55c6776d
Daniel Fernández Roldán <danielfernandezroldan2005@gmail.com>
2026-09-16 13:18:11 +0200

    Added a method for identifying plant receiving as a parameter the image.

--- backend/src/services/gemini.service.js
+++ backend/src/services/gemini.service.js
@@ -34,5 +34,28 @@ export class GeminiService {
   // Asyncron Method which receive image converted in Base64.
   async identifyPlant(imagePart) {
     // TODO: Implement AI call.
+    // Create the prompt for Gemini.
+    const prompt = "Act as an expert botanist. Identify this plant and provide the details strictly following the JSON schema.";
+    
+    // Initialize the Model.
+    const model = this.ai.getGenerativeModel({
+      model: "gemini-1.5-flash", // Fastest visual model.
+      generationConfig: { // Inject our schema to force a JSON output.
+        responseMimeType: "application/json", // Type of the response.
+        responseSchema: plantPokedexSchema // Inject structure of the JSON.
+      }
+    });
+
+    // Generate the content.
+    // Send an array containing both the text prompt and the image part.
+    // Remember to use 'await' because this calls the internet.
+    const result = await model.generateContent([prompt, imagePart]);
+
+    // Parse and return.
+    // Extract the raw text string from the response.
+    const rawText = result.response.text()
+
+    // Convert that JSON string into a usable JavaScript object and return it.
+    return JSON.parse(rawText);
   }
-}
+};
